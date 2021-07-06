596a6b0088c0210f23de0818c0d034a8788bbb82
yeahfinethx <80649612+yeahfinethx@users.noreply.github.com>
2021-07-06 22:11:51 +0100

    Add files via upload
    v03
    =========
    - Player can choose how many rounds to play
    - Player can choose from three stats to compare
    - Running score is kept
    - Player can choose whether to play another set of rounds
    =====
    Not finished
    - Pokemon are not repeated until all numbers are exhausted 1-151
    - Validation for all inputs

---
+++ Project_Pokemonv03.py
@@ -0,0 +1,130 @@
+# Project Brief: Top Trumps
+
+# In this project you'll create a small game where players compare stats, similar to the Top Trumps card game. The basic flow of the games is:
+    # You are given a random card with different stats
+    # You select one of the card's stats
+    # Another random card is selected for your opponent (the computer)
+    # The stats of the two cards are compared
+    #The player with the stat higher than their opponent wins
+# The standard project will use the Pokemon API, but you can use a different API if you want after completing the required tasks.
+
+# Required Tasks
+# These are the required tasks for this project. You should aim to complete these tasks before adding your own ideas to the project.
+    # Generate a random number between 1 and 151 to use as the Pokemon ID number
+
+# First, we import the two types of libraries we are using, random (for random integers) and requests (for accessing the PokeAPI)
+import random
+import requests
+
+# As a modification to the assignment, we ask the player how many rounds they would like to play using INPUT and force it into an integer using INT.
+# Additionally, we validate the response and return an error if the value returned is not an integer using a TRY function embedded in a WHILE loop. See https://www.w3schools.com/python/python_try_except.asp for more information.
+while True:
+    try:
+        rounds = int(input("How many rounds of PokeTrumps would you like to play? "))
+        print()
+    except ValueError:
+        print("Sorry, please enter a numerical value.\n")
+        continue
+    else:
+        break
+
+# We defined player scores here, outside of any functions, so we can add to these base scores later.
+p1score = 0
+p2score = 0
+
+# We defined a function in the form of gameround(), which allows us to call gameround() each time we want to start a new round.
+def gameround():
+    # We have left unfinished code intentions here. We are calling a random sample of two numbers and assigning one each to the players, but our original goal was to use this to ensure we don't repeat the same number twice.
+    deck = random.sample(range(1,151),2)
+    player1 = deck[0]
+    # We're using global here to declare to the code that even thought p1score and p2score are in this function, they are globally referenced throughout our code.
+    global p1score
+    global p2score
+
+    # Using the Pokemon API get a Pokemon based on its ID number
+    url1 = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player1)
+    response_p1 = requests.get(url1)
+    pokemon_p1 = response_p1.json()
+
+    # Create a dictionary that contains the returned Pokemon's name, id, height and weight (★https://pokeapi.co/)
+    player1_card = {
+        'name' : pokemon_p1['name'],
+        'id' : pokemon_p1['id'],
+        'height' : pokemon_p1['height'],
+        'weight' : pokemon_p1['weight'],
+        }
+    # Worth calling out some fancy footwork here that Rob helped us with. The formatting below is how we've gotten the output to display so nicely for the Pokemon's stats.
+    print("Your Pokemon for this round is: {}.\nID:{}\nHEIGHT:{}\nWEIGHT:{}\n".format(player1_card['name'].title(),player1_card['id'],player1_card['height'],player1_card['weight']))
+
+
+    # Get a random Pokemon for the player and another for their opponent
+    player2 = deck[1]
+
+    url2 = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(player2)
+    response_p2 = requests.get(url2)
+    pokemon_p2 = response_p2.json()
+    player2_card = {
+        'name' : pokemon_p2['name'],
+        'id' : pokemon_p2['id'],
+        'height' : pokemon_p2['height'],
+        'weight' : pokemon_p2['weight'],
+        }
+
+    print("Your opponent has {}.\n".format(player2_card['name'].title()))
+    # Ask the user which stat they want to use (id, height or weight)
+    # Modification (unfinished) - we are in the process of trying to validate the response from the input
+    while True:
+        try:
+            pokestat = input("Which stat do you want to compare? height, weight, id: ")
+        except pokestat not in [player1_card]:
+            print("Sorry, type one of the following words: id, height, weight.\n")
+            continue
+        else:
+            break
+
+
+    # Here we are defining variables for comparison and assigning them to player1_card using the stat the player chose earlier in the game.
+    pokestat_p1 = player1_card[pokestat]
+    pokestat_p2 = player2_card[pokestat]
+
+    # Compare the player's and opponent's Pokemon on the chosen stat to decide who wins
+    if pokestat_p1 > pokestat_p2:
+        print("You win round {}! Your {} stat of {} beat player two's {}.".format(round, pokestat, pokestat_p1, pokestat_p2))
+        p1score = p1score+1
+        print("The score is now P1: {} - P2: {}\n".format(p1score,p2score))
+    elif pokestat_p1 == pokestat_p2:
+        print("You tied round {}! Your {} stat of {} is the same as player two's {}.".format(round, pokestat, pokestat_p1, pokestat_p2))
+        print("The score is now P1: {} - P2: {}\n".format(p1score,p2score))
+    else:
+        print("You lost round {}! Your {} stat of {} did not beat player two's {}.".format(round, pokestat, pokestat_p1, pokestat_p2))
+        p2score = p2score+1
+        print("The score is now P1: {} - P2: {}\n".format(p1score,p2score))
+
+# Ugh I can't remember what we did here. Laura? Kharina? Lil help?
+def totalrounds():
+    global round
+    for round in range(rounds):
+        round = round+1
+        gameround()
+    print("============================================")
+    again = input("Would you like to play again? y/n ")
+
+    #If the player says yes, we run another set of rounds equal to the number the player chose at the beginning.
+    if again == 'y':
+        totalrounds()
+    else:
+        print("Thanks for playing!")
+
+totalrounds()
+
+
+
+# Ideas for Extending the Project
+# Here are a few ideas for extending the project beyond the required tasks. These ideas are just suggestions, feel free to come up with your own ideas and extend the program however you want.
+    # Use different stats for the Pokemon from the API
+    # Get multiple random Pokemon and let the player decide which one that they want to use
+    # Play multiple rounds and record the outcome of each round. The player with most number of rounds won, wins the game
+    # Allow the opponent (computer) to choose a stat that they would like to compare
+    # Record high scores for players and store them in a file
+    # Use a different API (see suggestions below)
+
